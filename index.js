4696384c7306b9f7a02fe4d42db6c3ca4df22ca8
Marcelo Laos <mlaos1997@gmail.com>
2021-04-18 21:50:23 -0400

    completed advanced array methods

--- index.js
+++ index.js
@@ -113,9 +113,15 @@ Use animalNames to populate and return the displayNames array with only the anim
 displayNames will be an array of strings, and each string should follow this pattern: "name: {name}, scientific: {scientific name}"
 */
 
-function animalNames( /*Your Code Here*/ ) {
-  /*Your Code Here*/
-}
+function animalNames(arr) {
+  let displayNames;
+  displayNames = arr.map((item) => {
+    return `name: ${item.animal_name}, scientific: ${item.scientific_name}`
+  });
+  return displayNames;
+};
+
+console.log(animalNames(zooAnimals));
 
 
 /* 🦁🦁🦁 Request 2: .map() 🦁🦁🦁
@@ -124,9 +130,14 @@ Using lowerCaseNames use .map() to create a new array of strings with the animal
 For example: ['jackal, asiatic', .....]
 */
 
-function lowerCaseNames( /*Your Code Here*/ ) {
-  /*Your Code Here*/
+function lowerCaseNames(arr) {
+  let lowerCaseNames;
+  lowerCaseNames = arr.map((item) => {
+    return item.animal_name.toLowerCase()
+  });
+  return lowerCaseNames;
 }
+console.log(lowerCaseNames(zooAnimals));
 
 
 /* 🦁🦁🦁 Request 3: .filter() 🦁🦁🦁
@@ -134,9 +145,10 @@ The zoo is concerned about animals with a lower population count.
 Using lowPopulationAnimals use .filter() to create a new array of objects which contains only the animals with a population of less than 5.
 */
 
-function lowPopulationAnimals( /*Your Code Here*/ ) {
-  /*Your Code Here*/
+function lowPopulationAnimals(arr) {
+  return arr.filter(animal => animal.population < 5);
 }
+console.log(lowPopulationAnimals(zooAnimals));
 
 
 /* 🦁🦁🦁 Request 4: .reduce() 🦁🦁🦁
@@ -145,9 +157,11 @@ Using USApop find the total population from the zoos array using the .reduce() m
 Remember the reduce method takes two arguments: a callback (which itself takes two args - the accumulator and the item), and an initial value for the count.
 */
 
-function USApop( /*Your Code Here*/ ) {
-  /*Your Code Here*/
+function USApop(arr) {
+  const reducer = (total, animal) => total + animal.population;
+  return arr.reduce(reducer, 0);
 }
+console.log(USApop(zooAnimals));
 
 
 // 🦁🦁🦁 Callbacks 🦁🦁🦁  
